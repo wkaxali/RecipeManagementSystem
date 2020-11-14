daeb2dd463b2361ec147e9558b1e2a42f85c7aad
MySelf <wkaxali@gmail.com>
2020-11-14 08:57:44 +0500

    Sales Screen working basic , nit started update data

--- resources/views/enterSales.blade.php
+++ resources/views/enterSales.blade.php
@@ -5,6 +5,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" />
     <link href="/css/app.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" />
+    <!-- CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
+        integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.22/css/jquery.dataTables.css">
+
+    <link rel="stylesheet" type="text/css"
+        href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.13.1/css/bootstrap-select.min.css">
 
 
 <style>
@@ -206,7 +214,7 @@ background-color: red;
 
 </head>
 
-<body>
+<body onload="getAllMenuItems()">
     <header class="mt-5 ">
         <div class="container">
             <div class="row">
@@ -221,20 +229,18 @@ background-color: red;
             <div class="row">
                 <div class="col-md-4" style="margin-top: 8px;">
                     <label style="width: 88px;" for="Men-item">Menu Item</label>
-                    <select style="width: 177px;"  data-live-search="true" tabindex="null">
-                    <option>Beef Steak</option>
-                    <option>25484</option>
-                    <option>212121</option>
-                    <option>020121</option>
-                    <option>875454</option>
-                    <option>Rayan</option>
-                    <option>Naeem</option>
+                    <select style="width: 177px;"  data-live-search="true" tabindex="null" class="selectpicker form-control" id="Menus"
+                    
+                    onchange="getSaleAndRecipeCost()">
+                   
                 </select>
                 </div>
                 <div class="col-md-5">
-                    <label for="Quantity" style="width: 85px;">Quantity</label>
-                    <input type="text" class="form-control-1" name="Quantity" id="Quantity">   
-                      <button class="btn ml-5 btn-success">Add Sale</button>
+                    <label for="Quantity" style="width: 85px;" >Quantity</label>
+                    <input type="text" class="form-control-1" name="Quantity" id="qty">   
+                      <button class="btn ml-5 btn-success" onclick="AddRowInSalesTable()">Add Sale</button>
+                      <input type="text" class="form-control-1" name="salePrice" id="salePrice"> 
+                      <input type="text" class="form-control-1" name="recipeCost" id="recipeCost">  
                 </div>
                 <div class="col-lg-3">
                     <div class="input-group date">
@@ -258,42 +264,22 @@ background-color: red;
         <div class="row">
             <div class="col-md-8 ">
                 <div class="ent-table">
-                    <table class="table table-bordered">
+                    <table class="table table-bordered" id="SalesTable">
                         <thead>
                             <tr>
-                                <th></th>
+                                
                                 <th>Product Id</th>
                                 <th>Product Name</th>
                                 <th>Quantity</th>
                                 <th>Price</th>
                                 <th>Total</th>
+                                <th>RCPU</th>
+                                <th>RTC</th>
+                                <th></th>
                             </tr>
                         </thead>
                         <tbody>
-                            <tr>
-                                <td></td>
-                                <td>10</td>
-                                <td>Tea</td>
-                                <td>20</td>
-                                <td>300</td>
-                                <td>1000</td>
-                            </tr>
-                            <tr>
-                                <td></td>
-                                <td>10</td>
-                                <td>Tea</td>
-                                <td>20</td>
-                                <td>300</td>
-                                <td>1000</td>
-                            </tr>
-                            <tr>
-                                <td></td>
-                                <td>10</td>
-                                <td>Tea</td>
-                                <td>20</td>
-                                <td>300</td>
-                                <td>1000</td>
-                            </tr>
+                            
 
                         </tbody>
                     </table>
@@ -301,11 +287,11 @@ background-color: red;
             </div>
             <div class="col-md-4 offme-10">
                 <label for="g-total">Grand Total</label>
-                <input type="text" name="text" id="text" class="form-control-1"><br>
+                <input type="text"   class="form-control-1" id="TotalSales"><br>
                 <label for="g-total">Raw Items Consumed</label>
-                <input type="text" name="text" id="text" class="form-control-1"><br>
+                <input type="text"  class="form-control-1" id="TotalCost"><br>
                 <label for="g-total">Profit w/o Operational Const</label>
-                <input type="text" name="text" id="text" class="form-control-1"><br>
+                <input type="text"  class="form-control-1"id="profit"><br>
              
                     <button style="margin-left:100px;" class="btn btn-success up-btn">Update</button>
   
@@ -329,14 +315,136 @@ background-color: red;
   
     <!--begin::Global Config(global config for global JS scripts)-->
 
-    <!--end::Global Config-->
-    <!--begin::Global Theme Bundle(used by all pages)-->
-    <script src="assets/plugins/global/plugins.bundle.js?v=7.0.4"></script>
-    <script src="assets/plugins/custom/prismjs/prismjs.bundle.js?v=7.0.4"></script>
-    <script src="assets/js/scripts.bundle.js?v=7.0.4"></script>
-    <script src="assets/js/pages/crud/forms/widgets/bootstrap-datetimepicker.js?v=7.0.4"></script>
+ 
 
     <!--end::Global Theme Bundle-->
 </body>
+   <!--end::Global Config-->
+    <!--begin::Global Theme Bundle(used by all pages)-->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+
+<!-- jQuery and JS bundle w/ Popper.js -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous">
+</script>
+<script type="text/javascript"
+    src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.13.1/js/bootstrap-select.min.js"></script>
+<script src="assets/js/script.js"></script>
+
+<script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.22/js/jquery.dataTables.js">
+</script>
+
+<script>
+
+
+function getAllMenuItems() {
+   
+   var xhttp = new XMLHttpRequest();
+ xhttp.onreadystatechange = function() {
+   if (this.readyState == 4 && this.status == 200) {
+       document.getElementById("Menus").innerHTML =
+     this.responseText;
+     $('#Menus').selectpicker('refresh');
+    // alert(this.responseText);
+     //searchRawMatirial();
+     //alert(this.responseText);
+   }
+ };
+ 
+ xhttp.open("GET", "./getAllMenuItems", true);
+ xhttp.send();
+};
+function AddRowInSalesTable() {
+  var table = document.getElementById("SalesTable");
+  var row = table.insertRow(-1);
+  
+  var cell1 = row.insertCell(0);
+  var cell2 = row.insertCell(1);
+  var cell3 = row.insertCell(2);
+  var cell4 = row.insertCell(3);
+  var cell5 = row.insertCell(4);
+  var cell6 = row.insertCell(5);
+  var cell7= row.insertCell(6);
+  var cell8=row.insertCell(7);
+  var e = document.getElementById("Menus");
+    var manuValue = e.options[e.selectedIndex].value;   
+    var manuText = e.options[e.selectedIndex].text;
+  cell1.innerHTML =  manuValue ;
+  cell2.innerHTML = manuText;
+  var qty=document.getElementById("qty").value;
+  var sp=document.getElementById("salePrice").value ;
+  var PUPP=document.getElementById("recipeCost").value
+  var totSale=sp*qty;
+  var totPurchase=PUPP*qty;
+  cell3.innerHTML =document.getElementById("qty").value ;
+  cell4.innerHTML = document.getElementById("salePrice").value ;
+  cell5.innerHTML = totSale ;
+  cell6.innerHTML = PUPP ;
+  cell7.innerHTML = totPurchase ;
+  
+  cell8.innerHTML = "<button onclick='deleteRow(this)' class=\"btn btn-danger \" style=\"height: 25px;width: 25px; padding:auto;\"></button>";
+  calc();
+}
+
+function getSaleAndRecipeCost(){
+    var PID=$('#Menus').find(":selected").val();
+    var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        var mydata = JSON.parse(this.response);
+       // alert( mydata[0]['SalePrice']);
+        document.getElementById("recipeCost").value=mydata[0]['RecipeCost'];
+        document.getElementById("salePrice").value=mydata[0]['SalePrice'];
+        
+      //  alert( mydata[0]['SalePrice']);
+
+      
+     // $( "#dataTable" ).removeAttr( "style" ).hide().fadeIn(1000);
+      
+     // calc();
+    }
+  };
+  var selectedValue=$('#Menus').find(":selected").val();
+
+  xhttp.open("GET", "./getSalePurchasePrice/"+PID, true);
+  xhttp.send();
+ 
+ }
+
+ function deleteRow(r) {
+  var i = r.parentNode.parentNode.rowIndex;
+  //alert(i);
+  document.getElementById("SalesTable").deleteRow(i);
+  calc();
+}
+
+
+function calc() {
+
+var table = document.getElementById('SalesTable');
+var rows = table.rows;
+var totalSale = 0;
+var totRecipeCost=0;
+var TotalSalescell;
+var TotalPCell;
+
+// Assume first row is headers, adjust as required
+// Assume last row is footer, addjust as required
+for (var i=1, iLen=rows.length ; i<iLen; i++) {
+    TotalSalescell = rows[i].cells[4];
+    TotalPCell= rows[i].cells[6];
+    totalSale += Number(TotalSalescell.textContent || TotalSalescell.innerText);
+    totRecipeCost+=Number(TotalPCell.textContent || TotalPCell.innerText);
+}
+document.getElementById('TotalSales').value = totalSale.toFixed(2);
+document.getElementById('TotalCost').value = totRecipeCost.toFixed(2);
+var profit=totalSale-totRecipeCost;
+document.getElementById('profit').value = profit.toFixed(2);
+}
+
+
+</script>
 
 </html>
